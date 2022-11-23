ed7def566dfd28c6fdfdb6dc8eacb95a03e42526
Wouter Geraedts <git@woutergeraedts.nl>
2022-11-23 19:21:00 +0100

    Rewrote to emit to MQTT

--- README.md
+++ README.md
@@ -1,21 +1,99 @@
 # slimmemeter
-Quick and dirty DSMR5 serial TTY to Prometheus exporter for your smart meter.
+Quick and dirty DSMR5 serial TTY to MQTT exporter for your smart meter.
 
-Will open a listening socket for the HTTP endpoints on `:9185`, and emit some default metrics for my home.
-You will probably need to adapt the program to make the specific metrics from your home visible.
-As such I do not recommend using this package.
-
-Will emit readings for high and low tariffs, current usage, voltage, gas usage from slave meter and number of incidents.
+Will emit readings for high and low tariffs, current usage, voltage, gas usage from slave meter and number of incidents to the `slimmemeter` topic as JSON.
 
 ## How to run
-Build it and upload the binary to the machine attached to your smart meter. You might need to cross compile for ARM if you're
-using a Raspberry Pi or similar. Then run using `./slimmemeter /dev/ttyUSB0`, or whatever you are using as a serial interface.
+Install the slimmemeter by running `cargo install slimmemeter`.
+Then run using `slimmemeter /dev/ttyUSB0`, or whatever you are using as a serial interface.
 
 DSMR5 smart meters invert the TX-pin, thus you might need to configure your serial interface (like FTDI) to adapt to this.
 You can also invert the signal yourself using a transistor, but that is a bit fiddly.
 
-## Cross compile (for ARM v6)
-```
-cargo install cross
-cross build --target arm-unknown-linux-gnueabihf
+## Example data
+```json
+{
+    "datetime": {
+        "year": 22,
+        "month": 11,
+        "day": 23,
+        "hour": 19,
+        "minute": 19,
+        "second": 52,
+        "dst": false
+    },
+    "meterreadings": [
+        {
+            "to": 6514.304,
+            "by": 0.0
+        },
+        {
+            "to": 7076.58,
+            "by": 0.0
+        }
+    ],
+    "tariff_indicator": [
+        0,
+        2
+    ],
+    "power_delivered": 0.352,
+    "power_received": 0.0,
+    "power_failures": 3,
+    "long_power_failures": 4,
+    "lines": [
+        {
+            "voltage_sags": 2,
+            "voltage_swells": 0,
+            "voltage": null,
+            "current": 1,
+            "active_power_plus": 0.353,
+            "active_power_neg": 0.0
+        },
+        {
+            "voltage_sags": 0,
+            "voltage_swells": 0,
+            "voltage": null,
+            "current": 0,
+            "active_power_plus": 0.0,
+            "active_power_neg": 0.0
+        },
+        {
+            "voltage_sags": 0,
+            "voltage_swells": 0,
+            "voltage": null,
+            "current": 0,
+            "active_power_plus": 0.0,
+            "active_power_neg": 0.0
+        }
+    ],
+    "slaves": [
+        {
+            "device_type": 3,
+            "meter_reading": [
+                {
+                    "year": 22,
+                    "month": 11,
+                    "day": 23,
+                    "hour": 19,
+                    "minute": 0,
+                    "second": 0,
+                    "dst": false
+                },
+                4877.691
+            ]
+        },
+        {
+            "device_type": null,
+            "meter_reading": null
+        },
+        {
+            "device_type": null,
+            "meter_reading": null
+        },
+        {
+            "device_type": null,
+            "meter_reading": null
+        }
+    ]
+}
 ```
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,27 @@
-use prometheus::{register_gauge, register_int_gauge};
-use prometheus_exporter;
-use std::env;
-use std::net::SocketAddr;
+use std::{path::PathBuf, time::Duration};
 
-fn main() {
+use clap::Parser;
+use rumqttc::{AsyncClient, ConnAck, ConnectReturnCode, MqttOptions, Packet, QoS};
+use tokio::task;
+
+#[derive(Parser)]
+struct Args {
+    #[clap()]
+    serial_path: PathBuf,
+    #[clap(long, default_value = "mqtt://localhost?client_id=slimmemeter")]
+    mqtt_url: String,
+}
+
+#[tokio::main]
+async fn main() {
+    env_logger::init();
+
+    let args = Args::parse();
+
     use serial::SerialPort;
 
-    let path = env::args_os()
-        .skip(1)
-        .next()
-        .expect("Please specify the serial TTY path");
-
-    let mut port = serial::open(&path)
-        .expect("Could not open serial TTY path");
+    let path = args.serial_path;
+    let mut port = serial::open(&path).expect("Could not open serial TTY path");
 
     port.reconfigure(&|settings| {
         settings.set_baud_rate(serial::Baud115200)?;
@@ -24,123 +33,54 @@ fn main() {
     })
     .expect("Failed to configure serial TTY port");
 
-    port.set_timeout(std::time::Duration::from_millis(2000))
+    port.set_timeout(std::time::Duration::from_millis(11000))
         .unwrap();
 
-    let addr_raw = "0.0.0.0:9185";
-    let addr: SocketAddr = addr_raw.parse()
-        .expect("Can not parse listen addr");
-
-    let exporter = prometheus_exporter::start(addr)
-        .expect("Could not start the prometheus exporter");
-
-    let power_delivered = register_gauge!(
-        "power_delivered",
-        "Power delivered in the last second (kWs)"
-    ).unwrap();
-    let power_received = register_gauge!(
-        "power_received",
-        "Power generated in the last second (kWs)"
-    ).unwrap();
-    let power_failures = register_int_gauge!(
-        "power_failures",
-        "Number of power failures")
-    .unwrap();
-    let long_power_failures = register_int_gauge!(
-        "long_power_failures",
-            "Number of long power failures"
-    ).unwrap();
-    let meterreadings_tariff1_to = register_gauge!(
-        "meterreadings_tariff1_to",
-        "Total power consumed under tariff 1"
-    )
-    .unwrap();
-    let meterreadings_tariff2_to = register_gauge!(
-        "meterreadings_tariff2_to",
-        "Total power consumed under tariff 2"
-    )
-    .unwrap();
-    let meterreadings_tariff1_by = register_gauge!(
-        "meterreadings_tariff1_by",
-        "Total power generated under tariff 1"
-    ).unwrap();
-    let meterreadings_tariff2_by = register_gauge!(
-        "meterreadings_tariff2_by",
-        "Total power generated under tariff 2"
-    ).unwrap();
-    let voltage_sags = register_int_gauge!(
-        "voltage_sags",
-        "Number of voltage sags"
-    ).unwrap();
-    let voltage_swells = register_int_gauge!(
-        "voltage_swells",
-        "Number of voltage swells"
-    ).unwrap();
-    let voltage = register_gauge!(
-        "voltage",
-        "Amount of voltage currently"
-    ).unwrap();
-    let current = register_int_gauge!(
-        "current",
-        "Amount of amperes currently"
-    ).unwrap();
-    let gas_delivered = register_gauge!(
-        "gas_delivered",
-        "Total gas delivered"
-    ).unwrap();    
+    let bytes = port.bytes().map(|b| b.unwrap());
 
     use std::io::Read;
-    let reader = dsmr5::Reader::new(
-        port.bytes().map(|b| b.unwrap())
-    );
+    let reader = dsmr5::Reader::new(bytes);
+
+    let mut options = MqttOptions::parse_url(args.mqtt_url).unwrap();
+    options.set_keep_alive(Duration::from_secs(5));
+
+    let (client, mut eventloop) = AsyncClient::new(options, 10);
+    log::info!("Connected to MQTT");
+
+    task::spawn(async move {
+        loop {
+            let notification = eventloop.poll().await.unwrap();
+
+            #[allow(clippy::single_match)]
+            match notification {
+                rumqttc::Event::Incoming(Packet::ConnAck(ConnAck {
+                    code: ConnectReturnCode::Success,
+                    ..
+                })) => {
+                    log::info!("Connected to MQTT");
+                }
+                _ => {}
+            }
+        }
+    });
 
     for readout in reader {
-        let guard = exporter.wait_request();
-
         let telegram = readout.to_telegram().unwrap();
         let state = dsmr5::Result::<dsmr5::state::State>::from(&telegram).unwrap();
 
-        power_delivered.set(state.power_delivered.unwrap());
-        power_received.set(state.power_received.unwrap());
-        power_failures.set(state.power_failures.unwrap() as i64);
-        long_power_failures.set(state.long_power_failures.unwrap() as i64);
-
-        meterreadings_tariff1_to.set(
-            state.meterreadings[dsmr5::Tariff::Tariff1 as usize]
-                .to
-                .unwrap(),
-        );
-        meterreadings_tariff2_to.set(
-            state.meterreadings[dsmr5::Tariff::Tariff2 as usize]
-                .to
-                .unwrap(),
-        );
-        meterreadings_tariff1_by.set(
-            state.meterreadings[dsmr5::Tariff::Tariff1 as usize]
-                .by
-                .unwrap(),
-        );
-        meterreadings_tariff2_by.set(
-            state.meterreadings[dsmr5::Tariff::Tariff2 as usize]
-                .by
-                .unwrap(),
+        log::info!(
+            "Received P1: delivered {:?}kW",
+            state.power_delivered.unwrap_or(0.)
         );
 
-        let line = &state.lines[dsmr5::Line::Line1 as usize];
-
-        voltage_sags.set(line.voltage_sags.unwrap() as i64);
-        voltage_swells.set(line.voltage_swells.unwrap() as i64);
-        voltage.set(line.voltage.unwrap());
-        current.set(line.current.unwrap() as i64);
-
-        let gas = &state.slaves[dsmr5::Slave::Slave1 as usize];
-
-        gas_delivered.set(gas.meter_reading.as_ref().unwrap().1);
-
-        // Notify exporter that all metrics have been updated so the caller client can
-        // receive a response.
-        drop(guard);
+        client
+            .publish(
+                "slimmemeter",
+                QoS::AtLeastOnce,
+                false,
+                serde_json::to_vec(&state).unwrap(),
+            )
+            .await
+            .unwrap();
     }
-
-    eprintln!("Could not parse data from reader");
 }
